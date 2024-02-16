b2c94ca0fd228739cee5e903b92ca74fb3aa267c
Borja García <debuti@gmail.com>
2024-02-16 01:06:37 +0100

    Added simple-lib

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# umock-examples
-Usage examples for debuti/umock
+# UMock examples
+
+Usage examples for [debuti/umock](https://github.com/debuti/umock)
+
+## License
+
+The examples in this repository are marked with [CC0 1.0](http://creativecommons.org/publicdomain/zero/1.0?ref=chooser-v1) 
---
+++ simple-lib/test/mylib.test.c
@@ -0,0 +1,44 @@
+#include <stdarg.h>
+#include <setjmp.h>
+#include <stddef.h>
+#include <cmocka.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mylib.h"
+#include "umock.h"
+
+MOCK(void *, malloc, trace_and_check, (size_t __size))
+{
+    printf("Mocked malloc!\n");
+    check_expected(__size);
+    return malloc(__size);
+}
+
+MOCK(void, free, trace, (void *__ptr))
+{
+    printf("Mocked free!\n");
+    free(__ptr);
+}
+
+static void mylib_test(void **state)
+{
+    struct mylib_st *opaque;
+
+    expect_value(MOCKNAME(malloc, trace_and_check), __size, 4);
+    MOCKUSE(malloc, trace_and_check);
+    MOCKUSE(free, trace);
+    assert_int_equal(mylib_init(&opaque), 0);
+    assert_int_equal(mylib_term(opaque), 0);
+}
+
+int main()
+{
+    const struct CMUnitTest tests[] =
+        {
+            cmocka_unit_test(mylib_test),
+        };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
